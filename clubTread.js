bd8ff0d5d3fb5628cdc9bdb35d485ff31d38030f
Joey Burning <joey.byrne@outlook.com>
2015-08-23 23:22:28 -0700

    add accounts package. Users can now login with username and password. Posts are displayed in simple list

--- clubTread.js
+++ clubTread.js
@@ -1,23 +1,86 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+Posts = new Mongo.Collection("Posts");
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+if (Meteor.isClient) {
+  //This code only runs on the client
+  Template.addPostForm.events({
+    'submit .new-post': function(event) {
+      event.preventDefault();
+
+      var title = event.target.title.value;
+      var body = event.target.body.value;
+
+      Meteor.call("addPost", title, body);
+
+      event.target.title.value = '';
+      event.target.body.value = '';
+    }
+  })
+
+  Template.body.helpers({
+    posts: function() {
+      return Posts.find({}, {sort: {createdAt: -1}});
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    Meteor.publish("post", function() {
+      return Posts.find({});
+    })
   });
 }
+
+Meteor.methods({
+  addPost: function(title, body) {
+    if(! Meteor.userId()) {
+      throw new Meteor.Error("sign in to post!");
+    }
+    Posts.insert({
+      title: title,
+      body: body,
+      owner: Meteor.userId(),
+      username: Meteor.user().username,
+      createdAt: new Date(),
+      comments: []
+    });
+  },
+  deletePost: function(postID) {
+    var post = Posts.findOne(postId);
+    if (Meteor.userId() !== post.owner) {
+      throw new Meteor.Error("not-authorized");
+    }
+    Posts.remove(postID);
+  },
+  updatePost: function(postID, title, body) {
+    var post = Posts.findOne(postID);
+    if (Meteor.userId() !== post.owner) {
+      throw new Meteor.Error("not-authorized");
+    }
+    db.post.update({
+      title: title,
+      body: body
+    });
+  },
+  writeComment: function(body, postID) {
+    var post = Posts.findOne(postID);
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("sign in to comment!");
+    }
+    db.post.update(
+      { 'post_id': postID },
+      { '$push': { 'comments': {
+        'posted': datetime.utcnow(),
+        'author': Meteor.userId(),
+        'body': body } } 
+      } 
+    )
+  }
+})
+
+  
